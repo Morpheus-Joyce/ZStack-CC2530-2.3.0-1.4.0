1bf50aeb68599a5d79bb3177d74be145cabf892a
Joyce <1048035455@qq.com>
2021-05-11 09:38:39 +0800

    周文瑞

---
+++ Projects/zstack/Samples/GenericApp/Source/Coordinator.c
@@ -0,0 +1,141 @@
+/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+* 文件名  ： Coordinator
+* 作者    ： Zhouwenrui,Suyuqi,Liuping,Panmeiqin
+* 版本    ： V1.0.0
+* 时间    ： 2021/5/10
+* 简要    ： 文件说明  
+********************************************************************
+* 副本
+*
+*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+/* 头文件 ----------------------------------------------------------------*/
+#include "OSAL.h"
+#include "AF.h"
+#include "ZDApp.h"
+#include "ZDObject.h"
+#include "ZDProfile.h"
+#include <string.h>
+#include "Coordinator.h"
+#include "DebugTrace.h"
+
+#if !defined( WIN32 )
+#include "OnBoard.h"
+#endif
+
+#include "hal_lcd.h"
+#include "hal_led.h"
+#include "hal_key.h"
+#include "hal_uart.h"
+/* 宏定义 ----------------------------------------------------------------*/
+/* 结构体或枚举 ----------------------------------------------------------*/
+const cId_t GenericApp_ClusterList[GENERICAPP_MAX_CLUSTERS] =
+{
+  GENERICAPP_CLUSTERID
+};
+
+
+const SimpleDescriptionFormat_t GenericApp_SimpleDesc =
+{
+  GENERICAPP_ENDPOINT,              
+  GENERICAPP_PROFID,                
+  GENERICAPP_DEVICEID,              
+  GENERICAPP_DEVICE_VERSION,        
+  GENERICAPP_FLAGS,                 
+  GENERICAPP_MAX_CLUSTERS,          
+  (cId_t *)GenericApp_ClusterList,  
+  0,          
+  (cId_t *)NULL   
+};
+
+
+endPointDesc_t GenericApp_epDesc;
+byte GenericApp_TaskID;   
+byte GenericApp_TransID; 
+
+/* 函数声明---------------------------------------------------------------*/
+void GenericApp_MessageMSGCB( afIncomingMSGPacket_t *pckt );
+void GenericApp_SendTheMessage( void );
+/* 外部变量引用 ----------------------------------------------------------*/
+
+/* 函数 ------------------------------------------------------------------*/
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* 函数名：GenericApp_Init
+* 参数：byte task_id
+* 返回：void
+* 作者：Zhouwenrui,Suyuqi,Liuping,Panmeiqin
+* 时间：2021/5/10
+* 描述：初始化函数
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void GenericApp_Init( byte task_id )
+{
+  GenericApp_TaskID = task_id;
+  GenericApp_TransID = 0;
+  GenericApp_epDesc.endPoint = GENERICAPP_ENDPOINT;
+  GenericApp_epDesc.task_id = &GenericApp_TaskID;
+  GenericApp_epDesc.simpleDesc
+            = (SimpleDescriptionFormat_t *)&GenericApp_SimpleDesc;
+  GenericApp_epDesc.latencyReq = noLatencyReqs;
+  afRegister( &GenericApp_epDesc );
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* 函数名：GenericApp_ProcessEvent
+* 参数：byte task_id, UINT16 events
+* 返回：UINT16
+* 作者：Zhouwenrui,Suyuqi,Liuping,Panmeiqin
+* 时间：2021/5/10
+* 描述：消息处理函数
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+UINT16 GenericApp_ProcessEvent( byte task_id, UINT16 events )
+{
+  afIncomingMSGPacket_t *MSGpkt;
+  if ( events & SYS_EVENT_MSG )
+  {
+    MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive( GenericApp_TaskID );
+    while ( MSGpkt )
+    {
+      switch ( MSGpkt->hdr.event )
+      {
+        case AF_INCOMING_MSG_CMD:
+          GenericApp_MessageMSGCB( MSGpkt );
+          break;
+        default:
+          break;
+      }
+      osal_msg_deallocate( (uint8 *)MSGpkt );
+      MSGpkt = (afIncomingMSGPacket_t *)osal_msg_receive( GenericApp_TaskID );
+    }
+    return (events ^ SYS_EVENT_MSG);
+  }
+  return 0;
+}
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* 函数名：GenericApp_MessageMSGCB
+* 参数：afIncomingMSGPacket_t *pkt
+* 返回：void
+* 作者：Zhouwenrui,Suyuqi,Liuping,Panmeiqin
+* 时间：2021/5/10
+* 描述：消息读取函数
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+void GenericApp_MessageMSGCB( afIncomingMSGPacket_t *pkt )
+{
+  unsigned char buffer[4] = "  ";
+  switch ( pkt->clusterId )
+  {
+    case GENERICAPP_CLUSTERID:
+     osal_memcpy(buffer,pkt->cmd.Data,3);
+
+     if((buffer[0] == 'L') || (buffer[1] == 'E') || (buffer[2] == 'D'))
+     {
+        HalLedBlink(HAL_LED_2,0,50,500);
+     }
+     else
+     {
+         HalLedSet(HAL_LED_2,HAL_LED_MODE_ON);
+     }
+    break;
+  }
+}
--- Projects/zstack/Samples/GenericApp/Source/OSAL_GenericApp.c
+++ Projects/zstack/Samples/GenericApp/Source/OSAL_GenericApp.c
@@ -23,7 +23,7 @@
   its documentation for any purpose.
 
   YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
-  PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+  PROVIDED “AS IS?WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
   INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
   NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
   TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
@@ -61,8 +61,8 @@
 #if defined ( ZIGBEE_FRAGMENTATION )
   #include "aps_frag.h"
 #endif
-
-#include "GenericApp.h"
+//#include "GenericApp.h"
+#include "Coordinator.h"
 
 /*********************************************************************
  * GLOBAL VARIABLES
@@ -126,6 +126,3 @@ void osalInitTasks( void )
 #endif
   GenericApp_Init( taskID );
 }
-
-/*********************************************************************
-*********************************************************************/
